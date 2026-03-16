9262647370fc809f3f606313eb2f4c37f98acd2f
rubiacharles164-wq <rubiacharles164@gmail.com>
2026-03-16 09:25:59 -0230

    point class

---
+++ Problem 2/point.java
@@ -0,0 +1,37 @@
+public class point {
+    private float x;
+    private float y;
+
+    public point(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+    }
+
+    public void setXY(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+    public float[] getXY() {
+        return new float[2]{x, y};
+    }
+
+    public String toString() {
+        return "(" + x + ", " + y + ")";
+    }
+}
