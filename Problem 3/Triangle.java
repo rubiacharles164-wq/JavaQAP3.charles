c0cf0d3bc104088a23276c5ea7c9696c7612c9be
rubiacharles164-wq <rubiacharles164@gmail.com>
2026-03-16 12:00:47 -0230

    Triangle sub class

---
+++ Problem 3/Triangle.java
@@ -0,0 +1,45 @@
+public class Triangle extends Shape {
+    private double a;
+    private double b;
+    private double c;
+
+    public Triangle(double a, double b, double c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    public double getA() {
+        return a;
+    }
+    public double getB() {
+        return b;
+    }
+    public double getC() {
+        return c;
+    }
+
+    public void setA(double a) {
+        this.a = a;
+    }
+    public void setB(double b) {
+        this.b = b;
+    }
+    public void setC(double c) {
+        this.c = c;
+    }
+
+    public double area() {
+        double s = (a + b + c) / 2;
+        return Math.sqrt(s * (s - a) * (s - b) * (s - c));
+    }
+
+    public double perimeter() {
+        return a + b + c;
+    }
+
+    public String toString() {
+        return "Triangle with sides " + a + ", " + b + ", " + c + " has area " + area() + " and perimeter " + perimeter();
+    }
+
+}
